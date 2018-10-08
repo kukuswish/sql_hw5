33c9f1f5f36f71a0e714fb409b8c04437948691f
STirso <kukuswish@gmail.com>
2018-10-08 14:31:20 -0400

    Questions 1a 1b

---
+++ sakila_dbhw.sql
@@ -0,0 +1,8 @@
+use sakila;
+
+-- 1a. Display the first and last names of all actors from the table actor.
+select first_name, last_name from actor;
+
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
+select concat(upper(first_name)," ", upper(last_name)) as 'Actor Name' from actor;
+
