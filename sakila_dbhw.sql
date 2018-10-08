185a0a0cb675ae15728092252f7d798401805e51
STirso <kukuswish@gmail.com>
2018-10-08 17:26:19 -0400

    Revision for question set 3

--- sakila_dbhw.sql
+++ sakila_dbhw.sql
@@ -22,12 +22,10 @@ select country_id, country.country from country where country.country in ('Afgha
 -- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
 -- so create a column in the table actor named description and use the data type BLOB 
 -- (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
-
-alter table `sakila`.`actor` add column `description` blob after `last_update`;
+alter table sakila.actor add column description blob after last_update;
 
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
-alter table `sakila`.`actor` 
-drop column `description`;
+alter table sakila.actor drop column description;
 
 -- 4a. List the last names of actors, as well as how many actors have that last name.
 select last_name, count(last_name) from actor group by last_name;
