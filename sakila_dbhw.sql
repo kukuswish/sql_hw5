1cb5f2c6abec6461e93a794a9bdd7e3564eb2a79
STirso <kukuswish@gmail.com>
2018-10-08 15:26:02 -0400

    Question set 6

--- sakila_dbhw.sql
+++ sakila_dbhw.sql
@@ -46,3 +46,34 @@ update actor set first_name='GROUCHO' where first_name='HARPO' limit 1;
 
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 show create table address;
+
+-- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
+select  staff.first_name, staff.last_name, address.address
+from staff 
+join address on staff.address_id = address.address_id;
+
+-- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+select staff.staff_id, staff.first_name, staff.last_name, sum(payment.amount) as 'August 2005'
+from staff
+join payment on staff.staff_id = payment.staff_id
+where payment_date like '2005-08%'
+group by staff.staff_id;
+
+-- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+select film.title, count(film_actor.actor_id) as 'Number of actor'
+from film
+join film_actor on film.film_id=film_actor.film_id
+group by film.film_id;
+
+-- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+select film.film_id, film.title, count(inventory.inventory_id) as 'Number of copies'
+from film
+join inventory on film.film_id = inventory.film_id
+where film.title = 'Hunchback Impossible';
+
+-- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+select customer.customer_id, customer.first_name, customer.last_name, sum(payment.amount) 
+from customer
+join payment on customer.customer_id = payment.customer_id
+group by customer.customer_id
+order by last_name asc;
